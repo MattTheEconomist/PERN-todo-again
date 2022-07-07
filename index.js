668b48f89c39000c499586286d545de0456bfbdb
Matt Ashford <mattashford1@gmail.com>
2022-07-07 12:32:39 -0400

    still works locally, changed port to process.env

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+
+// process.env.NODE_ENV
 
 //middleware
 app.use(cors());
 app.use(express.json()); //req.body
+
+app.use(express.static("client/build"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 //ROUTES//
 
@@ -82,6 +93,6 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("server has started on port 5000");
+app.listen(PORT, () => {
+  console.log(`server has started on port ${PORT}`);
 });
