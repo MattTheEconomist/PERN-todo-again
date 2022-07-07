a966df99fecb424a92bd665ea39574e276a315bf
Matt Ashford <mattashford1@gmail.com>
2022-07-07 12:51:06 -0400

    ready to add heroku

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
-const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); //req.body
 
-app.use(express.static("client/build"));
+// app.use(express.static("client/build"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
